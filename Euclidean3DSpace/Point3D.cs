f95443747b13f1da3cd12059069ce5be5e6996d5
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-06-17 17:24:58 +0300

    add Euclidean3DSpace app and Point3D struct

---
+++ Euclidean3DSpace/Point3D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euclidean3DSpace
+{
+    struct Point3D
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
