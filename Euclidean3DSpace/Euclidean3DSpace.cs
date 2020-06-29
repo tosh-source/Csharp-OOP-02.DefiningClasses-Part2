b61fcfc09394c980f1aedf3dd5db4d8e3113d8e1
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-06-29 22:26:50 +0300

    add two tests

--- Euclidean3DSpace/Euclidean3DSpace.cs
+++ Euclidean3DSpace/Euclidean3DSpace.cs
@@ -12,10 +12,11 @@ namespace Euclidean3DSpace
     {
         static void Main(string[] args)
         {
-            Point3D point3D = new Point3D() {X = 1, Y = 1, Z =  1};
+            Point3D point3D = new Point3D() {X = 1, Y = 2, Z = 3};
             Console.WriteLine(point3D);
             Console.WriteLine(Point3D.StartCoordinate);
             Console.WriteLine(ExtensionPointsOf3DSpace.CalculateDistance(point3D, Point3D.StartCoordinate));
+
         }
     }
 }
--- Euclidean3DSpace/Extensions/3DExtensionPoints.cs
+++ Euclidean3DSpace/Extensions/3DExtensionPoints.cs
@@ -22,3 +22,16 @@ namespace Euclidean3DSpace.Extensions
         }
     }
 }
+
+//TEST static class "CalculateDistance"
+//
+//Test-1
+//var point3D = new Point3D() {X = 1, Y = 1, Z = 1};
+//Console.WriteLine(ExtensionPointsOf3DSpace.CalculateDistance(point3D, Point3D.StartCoordinate));
+//result -> 1.73205080756888
+//
+//Test-2
+//var point3D = new Point3D() {X = 1, Y = 2, Z = 3};
+//Console.WriteLine(ExtensionPointsOf3DSpace.CalculateDistance(point3D, Point3D.StartCoordinate));
+//result -> 3.74165738677394
+
