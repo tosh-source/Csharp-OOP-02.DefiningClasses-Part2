cd9fb2e2c633c6ae701763e450cfc4b810361833
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-07-01 23:13:40 +0300

    work on subproblem PathStorage

---
+++ Euclidean3DSpace/TextFromFileReader/PathStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Euclidean3DSpace.Models;
+
+namespace Euclidean3DSpace.TextFromFileReader
+{
+    public static class PathStorage
+    {
+        private static string textFromFile;
+
+        public static void LoadPath(string fileReader)
+        {
+            var reader = new StreamReader(fileReader);
+            textFromFile = reader.ReadToEnd();
+            reader.Close();
+        }
+        public static void SafePath()
+        {
+
+        }
+    }
+}
