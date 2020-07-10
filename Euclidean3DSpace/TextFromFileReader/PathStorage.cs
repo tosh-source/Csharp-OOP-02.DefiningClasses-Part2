70560ec9a6b2e1185345d060a04e8dd49c419d2a
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-07-10 17:19:18 +0300

    add exception handler and properties

--- Euclidean3DSpace/TextFromFileReader/PathStorage.cs
+++ Euclidean3DSpace/TextFromFileReader/PathStorage.cs
@@ -11,12 +11,40 @@ namespace Euclidean3DSpace.TextFromFileReader
     public static class PathStorage
     {
         private static string textFromFile;
+        private static string textToFile;
+
+        public static string TextFromFile
+        {
+            get { return textFromFile; }
+            set { textFromFile = value; }
+        }
+        public static string TextToFile
+        {
+            get { return textToFile; }
+            set { textToFile = value; }
+        }
 
         public static void LoadPath(string filePath)
         {
-            var reader = new StreamReader(filePath, Encoding.UTF8);
-            textFromFile = reader.ReadToEnd();
-            reader.Close();
+            try
+            {
+                using (var reader = new StreamReader(filePath, Encoding.UTF8))
+                {
+                    textFromFile = reader.ReadToEnd();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File was not found!");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Wrong directory path!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The file can't be accessed, parsed or do not exist!");
+            }
         }
         public static void SafePath()
         {
