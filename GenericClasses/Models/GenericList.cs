caaa689cc27d56a5555f1f9e2096d5b60b3aefba
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-08-27 11:17:37 +0300

    add more classes

--- GenericClasses/Models/GenericList.cs
+++ GenericClasses/Models/GenericList.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GenericClasses
+namespace GenericClasses.Models
 {
-    class Program
+    class GenericList
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
