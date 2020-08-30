1a1ce59e2e41dd19b695453eca324a8ef461ca26
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-08-30 15:59:36 +0300

    implement IEmumerable_T through Elements

--- GenericClasses/Models/GenericList.cs
+++ GenericClasses/Models/GenericList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GenericClasses.Models
 {
-    class GenericList<T>
+    class GenericList<T> : IEnumerable<T>
     {
         private T[] elements;
         private int currentPosition = 0;
@@ -38,13 +39,30 @@ namespace GenericClasses.Models
         {
             if (index <= this.Elements.Length - 1)
             {
-                //var tempElement = ;
+                //var tempElement = element;
+                //var firstTempArrOfElements;
+
+                //foreach (var currentElement in Elements)
+                //{
+
+                //}
+
                 //Array.Copy();
             }
             else
             {
                 throw new ArgumentOutOfRangeException();
             }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)Elements).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)Elements).GetEnumerator();
         }
 
         public T this[int index]
