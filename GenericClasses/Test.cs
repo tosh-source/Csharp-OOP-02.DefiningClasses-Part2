cc83db4468b040151239b0f712adae1852153f1e
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-09-08 14:50:08 +0300

    add some tests

--- GenericClasses/Test.cs
+++ GenericClasses/Test.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GenericClasses.Models;
 
 namespace GenericClasses
 {
@@ -10,6 +11,14 @@ namespace GenericClasses
     {
         static void Main(string[] args)
         {
+            var genericList = new GenericList<int>(3);
+            genericList.Add(5);
+            genericList.Add(10);
+            genericList.Add(20);
+            Console.WriteLine(genericList);
+
+            genericList.Remove(1);
+            Console.WriteLine(genericList);
         }
     }
 }
