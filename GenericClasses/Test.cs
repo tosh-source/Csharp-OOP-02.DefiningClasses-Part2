491021dde52d0b3aa3b1cd87763b0beffabf9e8e
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-09-09 11:32:55 +0300

    add more tests

--- GenericClasses/Test.cs
+++ GenericClasses/Test.cs
@@ -11,16 +11,25 @@ namespace GenericClasses
     {
         static void Main(string[] args)
         {
+            //Create new Generic List of Integers and add some items.
             var genericList = new GenericList<int>(3);
             genericList.Add(5);
             genericList.Add(10);
             genericList.Add(20);
             Console.WriteLine(genericList);
 
+            //Test Remove method.
+            genericList.Remove(2);
+            Console.WriteLine(genericList);
             genericList.Remove(1);
             Console.WriteLine(genericList);
 
-            genericList.Insert(7878, 1);
+            //Test Exceptions.
+            //genericList.Insert(7878, 1);  //<- uncomment to test IndexOutOfRangeException
+            //genericList.Add(10);
+
+            //Test Insert method.
+            genericList.Insert(7878, 0);
             Console.WriteLine(genericList);
         }
     }
