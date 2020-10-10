a72bbbf5c7ab101d1a3acf0d1c9e8df6c0c8ebd5
Tosh (on V-PC) <alehandro_test@outlook.com>
2020-10-11 01:47:49 +0300

    add test file

---
+++ Matrices/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Matrices.Models;
+
+namespace Matrices
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            //Create new Matrix
+            var genericMatrix = new GenericMatrix<int>(2, 3);
+            
+        }
+    }
+}
