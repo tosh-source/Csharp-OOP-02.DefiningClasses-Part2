322dbe8238895d2e9b2988fb4cf38e6b85ca925f
Tosh (on V-PC) <alehandro_test@outlook.com>
2020-10-10 14:40:10 +0300

    Define fild, property and constructor of class Matrix.

---
+++ Matrices/Models/GenericMatrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    class GenericMatrix<T>
+    {
+        private T[,] matrix;
+
+        public T[,] Matrix { get; private set; }
+
+        public GenericMatrix(int rows, int cols)
+        {
+            this.Matrix = new T[rows, cols];
+        }
+    }
+}
