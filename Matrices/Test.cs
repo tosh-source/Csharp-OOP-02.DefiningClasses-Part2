ac32dd43f339361f9d0a12322b5c8ab8bf9c9e47
Tosh (on V-PC) <alehandro_test@outlook.com>
2020-10-12 02:00:48 +0300

    add some tests

--- Matrices/Test.cs
+++ Matrices/Test.cs
@@ -12,8 +12,12 @@ namespace Matrices
         static void Main(string[] args)
         {
             //Create new Matrix
-            var genericMatrix = new GenericMatrix<int>(2, 3);
-            
+            var genericMatrix = new GenericMatrix<int>(2, 2);
+            genericMatrix[0, 0] = 32;
+            genericMatrix[0, 1] = 65;
+            genericMatrix[1, 0] = 76;
+            genericMatrix[1, 1] = 82;
+
         }
     }
 }
