59348886cce0e222129be1facd74620c15f21bab
Tosh (on V-PC) <alehandro_test@outlook.com>
2020-10-18 01:42:41 +0300

    add more tests

--- Matrices/Test.cs
+++ Matrices/Test.cs
@@ -13,11 +13,18 @@ namespace Matrices
         {
             //Create new Matrix
             var genericMatrix = new GenericMatrix<int>(2, 2);
-            genericMatrix[0, 0] = 32;
-            genericMatrix[0, 1] = 65;
-            genericMatrix[1, 0] = 76;
-            genericMatrix[1, 1] = 82;
+            genericMatrix[0, 0] = 1;
+            genericMatrix[0, 1] = 2;
+            genericMatrix[1, 0] = 3;
+            genericMatrix[1, 1] = 4;
 
+            var secondGenericMatrix = new GenericMatrix<int>(2, 2);
+            secondGenericMatrix[0, 0] = 5;
+            secondGenericMatrix[0, 1] = 6;
+            secondGenericMatrix[1, 0] = 7;
+            secondGenericMatrix[1, 1] = 8;
+
+            var result = genericMatrix * secondGenericMatrix;
         }
     }
 }
