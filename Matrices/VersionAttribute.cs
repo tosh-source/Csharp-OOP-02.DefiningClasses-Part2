709e87c2c307463cc7e389eb74e3d4d30f44eea1
Tosh (on V-PC) <alehandro_test@outlook.com>
2020-10-22 02:50:22 +0300

    add Attributes

---
+++ Matrices/VersionAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    [AttributeUsage(AttributeTargets.Struct |
+      AttributeTargets.Class | AttributeTargets.Interface,
+      AllowMultiple = true)]
+
+
+    public class AuthorAttribute : System.Attribute
+
+    {
+
+        public string Name { get; private set; }
+
+
+
+        public AuthorAttribute(string name)
+
+        {
+
+            this.Name = name;
+
+        }
+
+    }
+}
